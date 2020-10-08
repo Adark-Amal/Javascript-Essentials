e0dd96b4e29f577781803009c7b00b1009477995
David Adarkwah <72516754+Adark-Amal@users.noreply.github.com>
2020-10-08 01:11:11 +0000

    Brought dropdowns and div variables outside function

--- miniproject.js
+++ miniproject.js
@@ -1,14 +1,14 @@
+// get the various dropdown menus
+let dropdowns = document.getElementsByName("dropdown");
+
+let div = document.getElementById("modify");            // assign div element to variable
+
 function change(){
-  // get the various dropdown menus
-  let dropdowns = document.getElementsByName("dropdown");
-  
   // iterate to view name of each dropdown and each value in dropdown menu
   for(let i = 0; i < dropdowns; i++)
   {
     let dropdownAttribute = dropdowns[i].getAttribute("id");   // assign dropdown property name to variable
     let dropdownValues = dropdowns[i].value;                // assign dropdown value to variable 
-    let div = document.getElementById("modify");            // assign div element to variable
-    
     div.style[dropdownAttribute] = dropdownValues;
   };
 };
